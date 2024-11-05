bd7c36192644a131d2f70de8e3da9c2589b40f40
Alexandrei <alexandreijohnvecinal@gmail.com>
2024-11-05 23:38:46 +0100

    Added new features for neovim-obsidian: - removed frontmatter lines from sync-scroll; - sync-scroll toggle (ToggleObsidianSyncScroll)

--- nvim/lazyvim/lua/custom/neovim-obsidian.lua
+++ nvim/lazyvim/lua/custom/neovim-obsidian.lua
@@ -1,18 +1,35 @@
 local M = {}
 local curl = require("plenary.curl")
 local last_cursor_line = -1 -- Initialize to an invalid line number
-local debounce_timeout = 300 -- Trottle timeout
+local debounce_timeout = 500 -- Trottle timeout
 local vault_path = "/home/alexandrei/Documents/Notes"
 local active = false
 local PORT = 9000
+local sync_scroll = true
+
+local function calculate_frontmatter_lines()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local count = 0
+  if #lines > 1 and lines[1]:match("^---") then
+    for i = 2, #lines do
+      count = count + 1
+      if lines[i]:match("^---") then
+        break
+      end
+    end
+  end
+  return count
+end
 
 -- collect neovim data
 function M.get_cursor_data()
-  local line = vim.fn.getpos(".")[2]
+  local line = vim.api.nvim_win_get_cursor(0)[1]
   return {
     method = "updateCursor",
     params = {
       line = line,
+      totalLines = Total_lines,
+      frontmatterLines = FrontmatterLines + 3,
     },
   }
 end
@@ -53,7 +70,7 @@ end
 
 -- on action
 function M.on_cursor_moved()
-  if M.is_in_vault() and active then
+  if M.is_in_vault() and active and sync_scroll then
     local current_line = vim.fn.getpos(".")[2]
 
     if current_line ~= last_cursor_line then
@@ -68,6 +85,8 @@ end
 
 function M.on_buf_enter()
   if M.is_in_vault() and active then
+    FrontmatterLines = calculate_frontmatter_lines()
+    Total_lines = vim.fn.line("$")
     M.send_request(M.get_buffer_data())
   end
 end
@@ -92,18 +111,31 @@ function M.toggle()
   end
 end
 
+function M.toggle_sync_scroll()
+  sync_scroll = not sync_scroll
+  if sync_scroll then
+    print("ObsidianSyncScroll enabled")
+  else
+    print("ObsidianSyncScroll disabled")
+  end
+end
+
+-- autocmds
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
   pattern = "*.md",
   callback = M.on_cursor_moved,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
   pattern = "*.md",
   callback = M.on_buf_enter,
 })
 
+-- user commands
 vim.api.nvim_create_user_command("ToggleObsidianSync", M.toggle, {})
+vim.api.nvim_create_user_command("ToggleObsidianSyncScroll", M.toggle_sync_scroll, {})
 
+-- keymaps
 vim.keymap.set("n", "<leader>co", M.toggle, { desc = "ToggleObsidianSync", noremap = true, silent = true })
 
 return M
