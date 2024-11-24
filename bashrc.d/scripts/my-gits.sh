2eaf4a95c979d30bffefa9719767cd7b4b71db39
Alexandrei <alexandreijohnvecinal@gmail.com>
2024-11-24 20:07:31 +0100

    Improved my-gits searching time

--- bashrc.d/scripts/my-gits.sh
+++ bashrc.d/scripts/my-gits.sh
@@ -3,8 +3,13 @@
 my-gits() {
   local github_username="4lexandrei"
   local github_urls="https://github.com/$github_username|git@github.com:$github_username"
+  # Specify common directories with .git
+  local dirs=(
+    ~/.dotfiles
+    ~/dev
+  )
 
-  select_repo=$(find ~ -type f -path '*/.git/config' -exec grep -lE "url = ($github_urls)" {} + |
+  select_repo=$(find "${dirs[@]}" -type f -path '*/.git/config' -exec grep -lE "url = ($github_urls)" {} + |
     sed 's|/\.git/config||' |
     fzf)
 
